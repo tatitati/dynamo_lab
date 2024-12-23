0aaaa1ed33de4e20f8e60dd1b0f51ff552997973
tatitati <albertf@ryanair.com>
2024-12-23 21:16:26 +0100

    more

--- tests/test_partiql.py
+++ tests/test_partiql.py
@@ -1,11 +1,43 @@
+import os
+
 import boto3
 
-def test_partiql():
+def test_partiql_query():
+    os.environ['AWS_PROFILE'] = 'dev-legacy'
     client = boto3.client("dynamodb")
 
     response = client.execute_statement(
-        Statement="SELECT * FROM mydynamotable WHERE id = 'John'"
+        Statement="SELECT * FROM mydynamotable WHERE email = 'newemail@company.org'"
     )
 
     for item in response["Items"]:
         print(item) # {'email': {'S': 'newemail@company.org'}, 'id': {'S': 'John'}}
+
+
+def test_partiql_insert():
+    os.environ['AWS_PROFILE'] = 'dev-legacy'
+    client = boto3.client("dynamodb")
+
+    client.execute_statement(
+        Statement=f"INSERT INTO \"mydynamotable\" VALUE {{'id': ?, 'email': ?, 'age': ?}}",
+        Parameters=[
+            {'S': 'julius2'},
+            {'S': 'secondemail@whatever.es'},
+            {'N': '24'},
+        ]
+    )
+
+def test_partiql_update():
+    os.environ['AWS_PROFILE'] = 'dev-legacy'
+    client = boto3.client("dynamodb")
+
+    result = client.execute_statement(
+
+        Statement=f'UPDATE \"mydynamotable\" SET email=? WHERE id=? AND age=?',
+        Parameters=[
+            {'S': 'updated_email@update.es'},
+            {'S': 'julius2'},
+            {'N': '24'},
+        ]
+    )
+
